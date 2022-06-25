b0482462c191c57f1d42b449daa4df75cd22c2fb
Samuel Fabián <sfabian1996@gmail.com>
2022-06-24 21:35:16 -0300

    Adicionado música e movimento da câmera

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float speed = 10f;
+    public Transform player;
+
+    void LateUpdate(){
+        Vector3 newPosition = player.position;
+        newPosition.z = -10;
+        
+        transform.position = Vector3.Slerp(transform.position, newPosition, speed * Time.deltaTime);
+    }
+}
