893a71116b3894acb43810ca83a688c8750ad750
micheleromao <michelle.nunes10@gmail.com>
2022-07-11 01:02:56 -0300

    add student sprite

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,25 +5,81 @@ using UnityEngine.TestTools;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float jumpSpeed = 17f;
-    public float moveSpeed = 20f;
+    public float jumpSpeed = 50f;
+    public float moveSpeed = 5f;
     [SerializeField] private LayerMask platformLayerMask;
     private GameObject playerObject;
     private Rigidbody2D rigidBody2D;
     private BoxCollider2D boxCollider2D;
-    
+
+    private Animator animate;
+    float inputHorizontal;
+    float inputVertical;
+    bool facingRight = true;
+
+    private float jumpForce;
+    private bool isJumping;
+    private float moveHorizontal;
+    private float moveVertical;
+
     // Start is called before the first frame update
     private void Start()
     {
         playerObject = GetComponent<GameObject>();  
         rigidBody2D = GetComponent<Rigidbody2D>();
         boxCollider2D = GetComponent<BoxCollider2D>();
+        animate = gameObject.GetComponent<Animator>();
+
+        jumpForce = 60f;
+        isJumping = false;
     }
 
     // Update is called once per frame
     private void Update()
     {
         HandleMovement();
+
+        moveVertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate(){
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        inputVertical = Input.GetAxisRaw("Vertical");
+
+        if(inputHorizontal > 0 && !facingRight){
+            Flip();
+        }
+         if(inputHorizontal < 0 && facingRight){
+            Flip();
+        }
+
+        if (!isJumping && moveVertical > 0.1f){
+            rigidBody2D.AddForce(new Vector2(0f, moveVertical * jumpForce), ForceMode2D.Impulse);
+            animate.SetBool("isJump", true);
+        }else{
+            animate.SetBool("isJump", false);
+            
+        }
+    }
+
+    void Flip(){
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        facingRight = !facingRight;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision){
+        if(collision.gameObject.tag == "Platform"){
+            isJumping = false;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision){
+        if(collision.gameObject.tag == "Platform"){
+            isJumping = true;
+        }
     }
 
     private bool IsGrounded()
@@ -35,8 +91,12 @@ public class PlayerMovement : MonoBehaviour
 
     private void HandleJump()
     {
-        if(IsGrounded() && Input.GetKeyDown(KeyCode.UpArrow))
-            rigidBody2D.velocity = Vector2.up * jumpSpeed; 
+        if(IsGrounded() && Input.GetKeyDown(KeyCode.UpArrow)){
+            animate.SetBool("isJump", true);
+            rigidBody2D.velocity = Vector2.up * jumpSpeed;
+        }
+            animate.SetBool("isJump", false);
+
     }
 
     private void HandleRun()
@@ -47,20 +107,24 @@ public class PlayerMovement : MonoBehaviour
     private void HandleMovement()
     {
         HandleRun();
-        HandleJump();
+        //HandleJump();
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rigidBody2D.velocity = new Vector2(-moveSpeed, rigidBody2D.velocity.y);
+            animate.SetBool("isRun", true);
         }
         else
         {
             if (Input.GetKey(KeyCode.RightArrow))
             {
                 rigidBody2D.velocity = new Vector2(+moveSpeed, rigidBody2D.velocity.y);
+                animate.SetBool("isRun", true);
+
             }
             else
             {
                 rigidBody2D.velocity = new Vector2(0, rigidBody2D.velocity.y);
+                animate.SetBool("isRun", false);
             }
         }
     }
